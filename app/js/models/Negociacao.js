5b8fe99181acd3f1a7544f518e47e1ec3384ab92
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-02-28 13:53:47 -0300

    readonly properties

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -5,23 +5,15 @@ System.register([], function (exports_1, context_1) {
     return {
         setters: [],
         execute: function () {
+            // readonly é util quando se tem propriedades que só vao receber valor uma vez e depois disso não podem ser alteradas, somente visualizadas
             Negociacao = class Negociacao {
-                constructor(_data, _quantidade, _valor) {
-                    this._data = _data;
-                    this._quantidade = _quantidade;
-                    this._valor = _valor;
-                }
-                get data() {
-                    return this._data;
-                }
-                get quantidade() {
-                    return this._quantidade;
-                }
-                get valor() {
-                    return this._valor;
+                constructor(data, quantidade, valor) {
+                    this.data = data;
+                    this.quantidade = quantidade;
+                    this.valor = valor;
                 }
                 get volume() {
-                    return this._quantidade * this._valor;
+                    return this.quantidade * this.valor;
                 }
             };
             exports_1("Negociacao", Negociacao);
